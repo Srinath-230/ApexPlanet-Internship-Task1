b9ca43408dc849f86b0edd07ee5a319917ca07ff
Srinath-230 <srinathg092@gmail.com>
2025-08-01 17:34:56 +0530

    Initial commit for Task 1: Biryani Recipe Page.

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const myButton = document.getElementById('myButton');
+
+
+myButton.addEventListener('click', function() {
+    
+    alert('Cooking Tip: Always taste your Biryani before serving and adjust seasoning if needed! Enjoy!');
+});
